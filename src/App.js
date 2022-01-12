9cb6b1292a93732b78aa0d79344dd22b3f1342c5
Mbring jeremie <mbringjeremys@gmail.com>
2022-01-12 19:07:32 +0100

    frist component created with react framework

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class ToogleComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isToggle: false
+    };
+
+    this.setToogle = this.setToogle.bind(this); 
+  }
+
+  setToogle() {
+    this.setState(state => ({
+      isToggle: !state.isToggle
+    }));
+  }
+
+  render() {
+    const listName = ["mbring", "jeremie", "fouba", "fabrice","mahimi"]
+    return (
+      <div>
+        <button onClick={this.setToogle}>
+          {this.state.isToggle ? 'ON':'OF'}
+        </button>
+        <div>
+          <ul>
+            {this.state.isToggle && listName.map(list => <li key={list}>{list}</li>)}  
+          </ul>        
+        </div>
+      </div>
+    )
+  }
+
+} 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ToogleComponent />
       </header>
     </div>
   );

faa3d75d2ef265ce53987fb5db6d36515cec12be
Mbring jeremie <mbringjeremys@gmail.com>
2022-01-15 08:40:10 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,56 +1,15 @@
 import './App.css';
 import React from 'react';
-import { WelcomeDialog } from './components/Heritage'
-import { Apps } from './components/Heritage';
 import {Button} from './style/button';
-class ToogleComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggle: false
-    };
-
-    this.setToogle = this.setToogle.bind(this); 
-  }
-
-  setToogle() {
-    this.setState(state => ({
-      isToggle: !state.isToggle
-    }));
-  }
-
-  render() {
-    const listName = ["mbring", "jeremie", "fouba", "fabrice","mahimi"]
-    return (
-      <div>
-      <Button primary="blue">button styled</Button>
-      <Button>
-        <button onClick={this.setToogle}>
-          {this.state.isToggle ? 'ON':'OF'}
-        </button>
-      </Button>
-      
-        <div>
-          <ul>
-            {this.state.isToggle && listName.map(list => <li key={list}>{list}</li>)}  
-          </ul>        
-        </div>
-      </div>
-    )
-  }
-
-} 
+import { render } from '@testing-library/react';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <ToogleComponent />
-        <WelcomeDialog />
-        <Apps />
-      </header>
+    <div>
+      <Button>button styled</Button>
     </div>
-  );
+  )
 }
 
 export default App;
